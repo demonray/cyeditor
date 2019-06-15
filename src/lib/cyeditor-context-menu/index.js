50e4ac9db47ff1c7fb1d791916942fc225cd6cd6
雷其榕 <ryanlei@webank.com>
2019-06-15 10:08:48 +0800

    contexmenu show,close,changeMenus

--- src/lib/cyeditor-context-menu/index.js
+++ src/lib/cyeditor-context-menu/index.js
@@ -6,15 +6,16 @@ const defaults = {
       id: 'remove',
       content: 'remove',
       disabled: false,
-      divider: false
+      divider: true
     },
     {
       id: 'hide',
       content: 'hide',
-      disabled: false,
-      divider: false
+      disabled: false
     }
-  ]
+  ],
+  beforeShow: () => { return true },
+  beforeClose: () => { return true }
 }
 
 class ContextMenu {
@@ -25,18 +26,25 @@ class ContextMenu {
     this._init()
   }
   _init () {
+    this._initContainer()
     this._initDom()
     this._initEvents()
   }
 
-  _initDom () {
+  _initContainer () {
     this._container = this.cy.container()
     this.ctxmenu = document.createElement('div')
     this.ctxmenu.className = 'cy-editor-ctx-menu'
     this._container.append(this.ctxmenu)
+  }
+
+  _initDom () {
     let domItem = ''
     this._options.menus.forEach(item => {
-      domItem += `<div class="ctx-menu-item" data-menu-item="${item.id}">${item.content}</div>`
+      domItem += `<div class="ctx-menu-item ${item.disabled ? 'ctx-menu-item-disabled' : ''}" data-menu-item="${item.id}">${item.content}</div>`
+      if (item.divider) {
+        domItem += '<div class="ctx-menu-divider" ></div>'
+      }
     })
     this.ctxmenu.innerHTML = domItem
   }
@@ -48,14 +56,12 @@ class ContextMenu {
       let top = renderedPos.y
       utils.css(this.ctxmenu, {
         top: top + 'px',
-        left: left + 'px',
-        display: 'block'
+        left: left + 'px'
       })
+      this.show(event)
     }
-    this._listeners.eventTapstart = () => {
-      utils.css(this.ctxmenu, {
-        display: 'none'
-      })
+    this._listeners.eventTapstart = (e) => {
+      this.close(e)
     }
     this._listeners.click = (e) => {
       const id = e.target.getAttribute('data-menu-item')
@@ -66,6 +72,60 @@ class ContextMenu {
     this.ctxmenu.addEventListener('mousedown', this._listeners.click)
     this.cy.on('cxttap', this._listeners.eventCyTap)
     this.cy.on('tapstart', this._listeners.eventTapstart)
+  }
+
+  disable (id, disabled = true) {
+    const items = utils.query(`.cy-editor-ctx-menu [data-menu-item=${id}]`)
+    items.forEach(menuItem => {
+      if (disabled) {
+        utils.addClass(menuItem, 'ctx-menu-item-disabled')
+      } else {
+        utils.removeClass(menuItem, 'ctx-menu-item-disabled')
+      }
+    })
+  }
+
+  changeMenus (menus) {
+    this._options.menus = menus
+    this._initDom()
+  }
+
+  show (e) {
+    if (typeof this._options.beforeShow === 'function' && !this.isShow) {
+      const show = this._options.beforeShow(e, this._options.menus.slice(0))
+      if (show === true) {
+        utils.css(this.ctxmenu, {
+          display: 'block'
+        })
+        this.isShow = true
+      } else if (show.then) {
+        show.then(() => {
+          utils.css(this.ctxmenu, {
+            display: 'block'
+          })
+          this.isShow = true
+        })
+      }
+    }
+  }
+
+  close (e) {
+    if (typeof this._options.beforeShow === 'function' && this.isShow) {
+      const close = this._options.beforeClose(e)
+      if (close === true) {
+        utils.css(this.ctxmenu, {
+          display: 'none'
+        })
+        this.isShow = false
+      } else if (close.then) {
+        close.then(() => {
+          utils.css(this.ctxmenu, {
+            display: 'none'
+          })
+          this.isShow = false
+        })
+      }
+    }
   }
 
   destroyCxtMenu () {
